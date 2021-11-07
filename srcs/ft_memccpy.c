78838933044bc52f895f2a7f21dd62641053317d
Severi <sseveri@gmail.com>
2021-11-08 01:51:55 +0200

    Added mem funcs

---
+++ srcs/ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: severi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/08 01:35:14 by severi            #+#    #+#             */
+/*   Updated: 2021/11/08 01:49:15 by severi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dest, const void *src, int c, size_t size)
+{
+	size_t	i;
+	char	*s;
+	char	*d;
+
+	i = 0;
+	s = (char *)src;
+	d = (char *)dest;
+	while (i < size)
+	{
+		d[i] = s[i];
+		if (d[i] == c)
+			return (dest + i);
+		i++;
+	}
+	return (NULL);
+}
