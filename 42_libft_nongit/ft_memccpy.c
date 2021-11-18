8020e78ce6f3a0f260aecb20409a1792974b2444
Severi <sseveri@gmail.com>
2021-11-19 00:10:57 +0200

    fixed few things

--- 42_libft_nongit/ft_memccpy.c
+++ 42_libft_nongit/ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: severi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 01:35:14 by severi            #+#    #+#             */
-/*   Updated: 2021/11/17 12:19:21 by ssavukos         ###   ########.fr       */
+/*   Updated: 2021/11/18 22:10:19 by severi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	{
 		d[i] = s[i];
 		if (s[i] == c)
-			return (&(dest[i + 1]));
+			return (dest);
 		i++;
 	}
 	return (NULL);
