c1f6d4c7bf249b5a339107e99040ad9b75fcc411
ssavukos <sseveri@gmail.com>
2021-12-07 11:25:19 +0200

    U. memchr

--- 42_libft_nongit/ft_memchr.c
+++ 42_libft_nongit/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: severi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 22:52:51 by severi            #+#    #+#             */
-/*   Updated: 2021/12/06 16:51:00 by severi           ###   ########.fr       */
+/*   Updated: 2021/12/07 11:17:19 by severi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t		i;
-	const char	*s_c;
+	size_t				i;
+	const unsigned char	*s_c;
 
 	i = 0;
-	s_c = (const char *)s;
+	s_c = (const unsigned char *)s;
 	while (i < n)
 	{
-		if ((unsigned char)s_c[i] == (unsigned char)c)
+		if (s_c[i] == (unsigned char)c)
 		{
 			return ((void *) s + i);
 		}
